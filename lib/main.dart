f33f2964050aef9b51629785f79d34b3acb1f357
Zokir Najmidinov <softdeveleng@gmail.com>
2021-03-30 09:54:45 +0500

    Revert "stateful widget test"
    This reverts commit ab90ed986f013393f0043297ee4438591f1621b6.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyFirstWidgetSF(),
+      home: MyFirstWidget(),
     );
   }
 }
@@ -113,25 +113,6 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class MyFirstWidget extends StatelessWidget {
-  int counter = 0;
-  @override
-  Widget build(BuildContext context) {
-    counter++;
-    print(counter);
-    return Container(
-      child: Center(
-        child: Text("Hello!"),
-      ),
-    );
-  }
-}
-
-class MyFirstWidgetSF extends StatefulWidget {
-  @override
-  _MyFirstWidgetSFState createState() => _MyFirstWidgetSFState();
-}
-
-class _MyFirstWidgetSFState extends State<MyFirstWidgetSF> {
   int counter = 0;
   @override
   Widget build(BuildContext context) {
