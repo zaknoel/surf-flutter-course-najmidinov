b78463056efb931cf9b8d50b7631f81764bbe1bf
Zak Noel <softdeveleng@gmail.com>
2021-04-08 10:24:09 +0500

    add scaffold creeen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyFirstWidget(),
-      title: "Test title",
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -21,18 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyFirstWidgetSF(),
+      home: SightListScreen(),
     );
   }
 }
