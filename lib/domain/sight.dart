a038d4c46896452cc910c94a11054b581936ff48
Zak Noel <softdeveleng@gmail.com>
2021-04-11 16:33:55 +0500

    Mock data

---
+++ lib/domain/sight.dart
@@ -0,0 +1,20 @@
+///model for Sights
+
+class Sight {
+  String name; //название достопримечательности
+  Location location; //координаты места
+  String url; //путь до фотографии в интернете
+  String details; //описание достопримечательности
+  Types type; //тип достопримечательности.
+  Sight({this.name, this.location, this.url, this.details, this.type});
+}
+
+//model for location coordinates
+class Location {
+  double lat;
+  double lon;
+  Location(lat, lon);
+}
+
+//enum for types of interesting places
+enum Types { city, museum, park, waterfall, mountain }
