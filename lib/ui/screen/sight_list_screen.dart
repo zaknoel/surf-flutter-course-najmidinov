dbe0a0002c7c2e154703ce43e06e4a7646618470
Zokir Najmidinov <softdeveleng@gmail.com>
2021-04-11 15:00:01 +0500

    task 1 (#8)

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+const TextStyle kTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 32,
+  fontStyle: FontStyle.normal,
+  fontFamily: "Roboto",
+  fontWeight: FontWeight.w700,
+);
+
 class SightListScreen extends StatefulWidget {
   SightListScreen({Key key}) : super(key: key);
 
@@ -11,21 +19,18 @@ class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        title: const Text('Interesting places'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hello!"),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Write someting..."),
-            ),
-          ],
+        automaticallyImplyLeading: false,
+        toolbarHeight: 150,
+        title: const Text(
+          "Список\nинтересных мест",
+          style: kTextStyle,
+          textAlign: TextAlign.start,
         ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
       ),
+      body: Container(),
     );
   }
 }
