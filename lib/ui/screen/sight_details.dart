6ff7014c202b3c4b2397a171a37401ac1748a89d
Zak Noel <softdeveleng@gmail.com>
2021-04-13 08:59:40 +0500

    small fix after homework checked

--- lib/ui/screen/sight_details.dart
+++ lib/ui/screen/sight_details.dart
@@ -14,27 +14,7 @@ class _SightDetailsState extends State<SightDetails> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Container(
-                  height: 360,
-                  color: Colors.teal[700], //image
-                ),
-                Positioned(
-                  top: 36,
-                  left: 16,
-                  child: Container(
-                    height: 32,
-                    width: 32,
-                    child: Icon(
-                      Icons.arrow_back_ios_outlined,
-                      size: 14,
-                    ),
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
+            DetailPhoto(),
             Container(
               padding: EdgeInsets.symmetric(vertical: 24, horizontal: 16),
               child: Column(
@@ -75,38 +55,7 @@ class _SightDetailsState extends State<SightDetails> {
                   SizedBox(
                     height: 24,
                   ),
-                  Container(
-                    height: 48,
-                    padding: EdgeInsets.all(15),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.green,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 15,
-                          width: 15,
-                          color: Colors.red,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'построить маршрут'.toUpperCase(),
-                          style: kDetailType.copyWith(
-                            fontSize: 14,
-                            letterSpacing: 0.3,
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ),
+                  RouteButton(),
                   SizedBox(
                     height: 24,
                   ),
@@ -114,61 +63,132 @@ class _SightDetailsState extends State<SightDetails> {
                   SizedBox(
                     height: 8,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 19,
-                              width: 22,
-                              color: Colors.red,
-                            ),
-                            SizedBox(
-                              width: 9,
-                            ),
-                            Text(
-                              'Запланировать',
-                              style: kDetailType.copyWith(
-                                  fontWeight: FontWeight.w400,
-                                  color: Color(0xff7C7E9256)),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 19,
-                              width: 22,
-                              color: Colors.red,
-                            ),
-                            SizedBox(
-                              width: 9,
-                            ),
-                            Text(
-                              'В Избранное',
-                              style: kDetailType.copyWith(
-                                fontWeight: FontWeight.w400,
-                                color: Color(0xff3B3E5B),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  )
+                  BottomButtons()
                 ],
               ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class BottomButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 19,
+                width: 22,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 9,
+              ),
+              Text(
+                'Запланировать',
+                style: kDetailType.copyWith(
+                    fontWeight: FontWeight.w400, color: Color(0xff7C7E9256)),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 19,
+                width: 22,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 9,
+              ),
+              Text(
+                'В Избранное',
+                style: kDetailType.copyWith(
+                  fontWeight: FontWeight.w400,
+                  color: Color(0xff3B3E5B),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class RouteButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      padding: EdgeInsets.all(15),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.green,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 15,
+            width: 15,
+            color: Colors.red,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            'построить маршрут'.toUpperCase(),
+            style: kDetailType.copyWith(
+              fontSize: 14,
+              letterSpacing: 0.3,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DetailPhoto extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 360,
+          color: Colors.teal[700], //image
+        ),
+        Positioned(
+          top: 36,
+          left: 16,
+          child: Container(
+            height: 32,
+            width: 32,
+            child: Icon(
+              Icons.arrow_back_ios_outlined,
+              size: 14,
+            ),
+            color: Colors.white,
+          ),
+        )
+      ],
     );
   }
 }
