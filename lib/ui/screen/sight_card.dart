ad796fc0567999cae8544bacbe6eaa42e028b0be
Zak Noel <softdeveleng@gmail.com>
2021-04-13 13:49:35 +0500

    homework4.8

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -9,80 +9,86 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 16),
-      height: 188,
-      child: Column(
-        children: [
-          Container(
-            height: 96,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-              color: Colors.teal[700],
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 16,
-                  left: 16,
-                  child: Text(typeNames[item.type], style: listPlaceTypeStyle),
+    return AspectRatio(
+      aspectRatio: 3 / 2,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 16),
+        height: 188,
+        child: Column(
+          children: [
+            Container(
+              height: 96,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
-                Positioned(
-                  top: 19,
-                  right: 18,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.red,
-                    ),
-                    height: 18,
-                    width: 20,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
+                color: Colors.teal[700],
               ),
-              color: kGreyColor,
-            ),
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 16,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Stack(
                 children: [
-                  Text(
-                    item.name,
-                    maxLines: 2,
-                    style: listTitleStyle,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
+                  Positioned(
+                    top: 16,
+                    left: 16,
+                    child:
+                        Text(typeNames[item.type], style: listPlaceTypeStyle),
                   ),
-                  SizedBox(
-                    height: 2,
+                  Positioned(
+                    top: 19,
+                    right: 18,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.red,
+                      ),
+                      height: 18,
+                      width: 20,
+                    ),
                   ),
-                  Text(
-                    item.details,
-                    maxLines: 1,
-                    style: listDescStyle,
-                    overflow: TextOverflow.ellipsis,
-                  )
                 ],
               ),
             ),
-          ),
-        ],
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                color: kGreyColor,
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 16),
+                    Text(
+                      item.name,
+                      maxLines: 2,
+                      style: listTitleStyle,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      item.details,
+                      maxLines: 1,
+                      style: listDescStyle,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
