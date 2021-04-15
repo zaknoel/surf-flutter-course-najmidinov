2334221a6647dbb4b2c6644d7adc24b1840b1a8a
Zokir Najmidinov <softdeveleng@gmail.com>
2021-04-15 08:27:52 +0500

    load images on list (#14)

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -20,14 +20,46 @@ class SightCard extends StatelessWidget {
               height: 96,
               width: double.infinity,
               decoration: BoxDecoration(
+                border: Border.all(color: kGreyColor),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
                 ),
-                color: Colors.teal[700],
+                //color: Colors.red[300],
               ),
               child: Stack(
                 children: [
+                  Container(
+                    width: double.infinity,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                      ),
+                      child: Image.network(
+                        item.url,
+                        fit: BoxFit.cover,
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+
+                          return Center(
+                            child: CircularProgressIndicator(
+                              backgroundColor: kYellowColor,
+                              strokeWidth: 5.0,
+                              valueColor: AlwaysStoppedAnimation(kGreenColor),
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes
+                                  : null,
+                            ),
+                          );
+                          // You can use LinearProgressIndicator or CircularProgressIndicator instead
+                        },
+                        errorBuilder: (context, error, stackTrace) =>
+                            Center(child: Text('Cannot load image, sorry!')),
+                      ),
+                    ),
+                  ),
                   Positioned(
                     top: 16,
                     left: 16,
@@ -52,6 +84,7 @@ class SightCard extends StatelessWidget {
             Container(
               width: double.infinity,
               decoration: BoxDecoration(
+                border: Border.all(color: kGreyColor),
                 borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(20),
                   bottomRight: Radius.circular(20),
